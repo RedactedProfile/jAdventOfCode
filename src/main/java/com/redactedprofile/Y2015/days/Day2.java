d2c15fb8d54b36c0f29486a81f593d790f3c5dc5
Kyle Harrison <redactedprofile@gmail.com>
2024-10-20 18:48:08 -0700

    2015:Day2:hard completed

--- src/main/java/com/redactedprofile/Y2015/days/Day2.java
+++ src/main/java/com/redactedprofile/Y2015/days/Day2.java
@@ -2,7 +2,6 @@ package com.redactedprofile.Y2015.days;
 
 import com.redactedprofile.AOCDay;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -38,16 +37,25 @@ public class Day2 extends AOCDay {
                         tokens -> calculateSurfaceArea((int) tokens.get(0), (int) tokens.get(2), (int) tokens.get(4),
                                 sqft::getAndAdd))));
 
-        System.out.printf("Total square footage of wrapping paper needed is " + sqft.get());
+        System.out.printf("Total square footage of wrapping paper needed is %dsqft%n", sqft.get());
     }
 
     /**
      * 2015:Day2:Hard
-     * The result of this puzzle is to do something, I don't know yet
+     * The result of this puzzle is to find the feet length of ribbon needed for tying and bowing
      */
     @Override
     public void hard() {
-        getInputLinesByLine(System.out::println);
+        assertTests();
+
+        AtomicInteger footage = new AtomicInteger(0);
+
+        getInputLinesByLine(
+                line -> tokenize(line, (
+                        tokens -> calculateRibbonLength((int) tokens.get(0), (int) tokens.get(2), (int) tokens.get(4),
+                                footage::getAndAdd))));
+
+        System.out.printf("Total footage length of ribbon needed is %dft%n", footage.get());
     }
 
 
@@ -95,9 +103,11 @@ public class Day2 extends AOCDay {
      * @return int Surface area + some slack
      */
     private int calculateSurfaceArea(int width, int height, int length) {
+        // find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l.
         return (2 * (length * width))  +
                (2 * (width  * height)) +
                (2 * (height * length)) +
+               // The elves also need a little extra paper for each present: the area of the smallest side.
                Stream.of((length * width), (width * height), (height * length))
                        .min(Comparator.naturalOrder())
                        .get();
@@ -113,6 +123,40 @@ public class Day2 extends AOCDay {
      */
     private void calculateSurfaceArea(int width, int height, int length, Consumer<Integer> consumer) {
         consumer.accept(calculateSurfaceArea(width, height, length));
+    }
+
+    /**
+     * Calculates the length of ribbon needed given the dimensions of a box, plus what's needed for the bow
+     * @param width
+     * @param height
+     * @param length
+     * @return int - length of ribbon. Includes bow.
+     */
+    private int calculateRibbonLength(int width, int height, int length) {
+        // The ribbon required to wrap a present is the shortest distance around its sides,
+        //      or the smallest perimeter of any one face.
+        List<Integer> values =
+            Stream.of(width, height, length)
+                    .sorted(Comparator.naturalOrder()).toList();
+
+        int out = (2 * values.get(0)) + (2 * values.get(1));
+
+        // Each present also requires a bow made out of ribbon as well; the feet of ribbon
+        // required for the perfect bow is equal to the cubic feet of volume of the present.
+        out += values.get(0) * values.get(1) * values.get(2);
+
+        return out;
+    }
+
+    /**
+     * Calculates the length of ribbon needed given the dimensions of a box, plus what's needed for the bow
+     * @param width
+     * @param height
+     * @param length
+     * @param consumer - length of ribbon. Includes bow.
+     */
+    private void calculateRibbonLength(int width, int height, int length, Consumer<Integer> consumer) {
+        consumer.accept(calculateRibbonLength(width, height, length));
     }
 
 
@@ -132,7 +176,13 @@ public class Day2 extends AOCDay {
         int assert3 = calculateSurfaceArea((int)assert1.get(0), (int)assert1.get(2), (int)assert1.get(4)),
             assert4 = calculateSurfaceArea((int)assert2.get(0), (int)assert2.get(2), (int)assert2.get(4));
 
-        if (assert3 != 58) throw new AssertionError(assert1str + " != 58, instead equals " + assert1);
-        if (assert4 != 43) throw new AssertionError(assert2str + " != 43, instead equals " + assert2);
+        if (assert3 != 58) throw new AssertionError("Box surface assertion " + assert1str + " != 58, instead equals " + assert3);
+        if (assert4 != 43) throw new AssertionError("Box surface assertion " + assert2str + " != 43, instead equals " + assert4);
+
+        int assert5 = calculateRibbonLength((int)assert1.get(0), (int)assert1.get(2), (int)assert1.get(4)),
+            assert6 = calculateRibbonLength((int)assert2.get(0), (int)assert2.get(2), (int)assert2.get(4));
+
+        if (assert5 != 34) throw new AssertionError("Ribbon length assertion " +assert1str + " != 34, instead equals " + assert5);
+        if (assert6 != 14) throw new AssertionError("Ribbon length assertion " +assert2str + " != 14, instead equals " + assert6);
     }
 }
