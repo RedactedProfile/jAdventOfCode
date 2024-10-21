30d3e79aeae5a601c998e9d52b0a1b3d3be9d3e3
Kyle Harrison <redactedprofile@gmail.com>
2024-10-20 18:23:24 -0700

    2015:Day2:easy comments

--- src/main/java/com/redactedprofile/Y2015/days/Day2.java
+++ src/main/java/com/redactedprofile/Y2015/days/Day2.java
@@ -24,6 +24,10 @@ public class Day2 extends AOCDay {
         return "2015/02.txt";
     }
 
+    /**
+     * 2015:Day2:Easy
+     * The result of this puzzle is to run through the entire input and add up all the discovered square footage
+     */
     @Override
     public void easy() {
         assertTests();
@@ -37,28 +41,21 @@ public class Day2 extends AOCDay {
         System.out.printf("Total square footage of wrapping paper needed is " + sqft.get());
     }
 
+    /**
+     * 2015:Day2:Hard
+     * The result of this puzzle is to do something, I don't know yet
+     */
     @Override
     public void hard() {
         getInputLinesByLine(System.out::println);
     }
 
-    private void assertTests() {
-        String assert1str = "2x3x4",
-               assert2str = "1x1x10";
-        List<Object> assert1 = tokenize(assert1str),
-                     assert2 = tokenize(assert2str);
-        if (((int) assert1.getFirst()) != 2) throw new AssertionError("String "+assert1str+", first result isn't 2, but " + assert1.getFirst());
-        if (((int) assert2.getFirst()) != 1) throw new AssertionError("String "+assert2str+", first result isn't 1, but " + assert2.getFirst());
-        int assert3 = calculateSurfaceArea((int)assert1.get(0), (int)assert1.get(2), (int)assert1.get(4)),
-            assert4 = calculateSurfaceArea((int)assert2.get(0), (int)assert2.get(2), (int)assert2.get(4));
-        if (assert3 != 58) throw new AssertionError(assert1str + " != 58, instead equals " + assert1);
-        if (assert4 != 43) throw new AssertionError(assert2str + " != 43, instead equals " + assert2);
-    }
 
     /**
      * This method finds and parses each of the integers found in the line
      * We also ignore anything else because it'll always be an x
      * @param line
+     * @return List of parsed values out of line
      */
     private List<Object> tokenize(String line) {
         Pattern pattern = Pattern.compile("\\d+|x");
@@ -79,6 +76,12 @@ public class Day2 extends AOCDay {
         return parsedTokens;
     }
 
+    /**
+     * This method finds and parses each of the integers found in the line
+     * We also ignore anything else because it'll always be an x
+     * @param line
+     * @param consumer supplies the List of Object's in a lambda
+     */
     private void tokenize(String line, Consumer<List<Object>> consumer) {
         consumer.accept(tokenize(line));
     }
@@ -100,7 +103,36 @@ public class Day2 extends AOCDay {
                        .get();
     }
 
+    /**
+     * Calculates the cubed surface area of a given W/H/L dimension and includes somme extra slack which
+     * is the extra area of the smallest side
+     * @param width
+     * @param height
+     * @param length
+     * @param consumer supplies the square footage result in a lambda
+     */
     private void calculateSurfaceArea(int width, int height, int length, Consumer<Integer> consumer) {
         consumer.accept(calculateSurfaceArea(width, height, length));
+    }
+
+
+    /**
+     * These are assertions to make sure all the calculations are arriving to the right values.
+     * If something's not right, we break the app.
+     */
+    private void assertTests() {
+        String assert1str = "2x3x4",
+               assert2str = "1x1x10";
+        List<Object> assert1 = tokenize(assert1str),
+                     assert2 = tokenize(assert2str);
+
+        if (((int) assert1.getFirst()) != 2) throw new AssertionError("String "+assert1str+", first result isn't 2, but " + assert1.getFirst());
+        if (((int) assert2.getFirst()) != 1) throw new AssertionError("String "+assert2str+", first result isn't 1, but " + assert2.getFirst());
+
+        int assert3 = calculateSurfaceArea((int)assert1.get(0), (int)assert1.get(2), (int)assert1.get(4)),
+            assert4 = calculateSurfaceArea((int)assert2.get(0), (int)assert2.get(2), (int)assert2.get(4));
+
+        if (assert3 != 58) throw new AssertionError(assert1str + " != 58, instead equals " + assert1);
+        if (assert4 != 43) throw new AssertionError(assert2str + " != 43, instead equals " + assert2);
     }
 }
