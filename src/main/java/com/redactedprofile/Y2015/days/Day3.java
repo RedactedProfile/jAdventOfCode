852bb6fc6b51862209a7bd38848450eee93f05fd
Kyle Harrison <redactedprofile@gmail.com>
2024-10-20 22:38:47 -0700

    2015:Day3 part 1 assertions

--- src/main/java/com/redactedprofile/Y2015/days/Day3.java
+++ src/main/java/com/redactedprofile/Y2015/days/Day3.java
@@ -2,14 +2,8 @@ package com.redactedprofile.Y2015.days;
 
 import com.redactedprofile.AOCDay;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.HashMap;
 import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 /**
  * 2015:Day3
@@ -17,8 +11,94 @@ import java.util.stream.Stream;
  */
 public class Day3 extends AOCDay {
 
+    class Point {
+        int x = 0, y = 0;
+        public void reset() { x = y = 0; }
+        public String encode() { return x + "," + y; }
+    }
+
+
     @Override
     public String getPuzzleInputFilePath() {
         return "2015/03.txt";
+    }
+
+
+    private void navigate(String directions, Consumer<String> directionCallback) {
+        String evaluatedDirection = "";
+        for(Character c : directions.toCharArray()) {
+            switch(c) {
+                case '^':
+                    evaluatedDirection = "up";
+                    break;
+                case 'v':
+                    evaluatedDirection = "down";
+                    break;
+                case '<':
+                    evaluatedDirection = "left";
+                    break;
+                case '>':
+                    evaluatedDirection = "right";
+                    break;
+                default:
+                    evaluatedDirection = "";
+                    break;
+            }
+            if(!evaluatedDirection.isBlank()) {
+                directionCallback.accept(evaluatedDirection);
+            }
+        }
+    }
+
+    private void updateMap(String direction, Point position, HashMap<String, Integer> map) {
+        switch (direction) {
+            case "up":
+                position.y += 1;
+                break;
+            case "down":
+                position.y -= 1;
+                break;
+            case "left":
+                position.x -= 1;
+                break;
+            case "right":
+                position.x += 1;
+                break;
+        }
+
+        String encodedPosition = position.encode();
+        if(map.containsKey(encodedPosition)) {
+            map.put(encodedPosition, map.get(encodedPosition) + 1);
+        } else {
+            map.put(encodedPosition, 1);
+        }
+    }
+
+    @Override
+    public void assertTests() {
+        String strTest1 = ">",
+               strTest2 = "^>v<",
+               strTest3 = "^v^v^v^v^v";
+
+        Point position = new Point();
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put(position.encode(), 1);
+        navigate(strTest1, (r -> updateMap(r, position, map)));
+
+        if (map.size() != 2) throw new AssertionError("Assertion 1 failed, size supposed to be 2 but is " + map.size());
+
+        position.reset();
+        map.clear();
+        map.put(position.encode(), 1);
+        navigate(strTest2, (r -> updateMap(r, position, map)));
+
+        if (map.size() != 4) throw new AssertionError("Assertion 2 failed, size supposed to be 4 but is " + map.size());
+
+        position.reset();
+        map.clear();
+        map.put(position.encode(), 1);
+        navigate(strTest3, (r -> updateMap(r, position, map)));
+
+        if (map.size() != 2) throw new AssertionError("Assertion 3 failed, size supposed to be 2 but is " + map.size());
     }
 }
