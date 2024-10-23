66f21315c0e4629948f4ab8882f257f87ae25c29
Kyle Harrison <redactedprofile@gmail.com>
2024-10-23 10:07:43 -0700

    2015:Day4 assertions

--- src/main/java/com/redactedprofile/Y2015/days/Day4.java
+++ src/main/java/com/redactedprofile/Y2015/days/Day4.java
@@ -2,9 +2,9 @@ package com.redactedprofile.Y2015.days;
 
 import com.redactedprofile.AOCDay;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Day4 extends AOCDay {
@@ -15,27 +15,55 @@ public class Day4 extends AOCDay {
     public Day4() throws NoSuchAlgorithmException {
     }
 
-    private void findLeadingPattern(String in, String pattern) {
-        digest.reset();
-        for(;;) {
-            var hash = digest.digest(in.getBytes()).toString();
+    private String md5(String in) {
+        StringBuilder md5Hex =
+                new StringBuilder(
+                    new BigInteger(1,
+                            digest.digest(in.getBytes()))
+                                .toString(16));
 
-            break;
+        // Pad with leading zeros if necessary
+        while (md5Hex.length() < 32) {
+            md5Hex.insert(0, "0");
+        }
+        return md5Hex.toString();
+    }
+
+    /**
+     *
+     * @param in
+     * @param pattern
+     * @return number suffix or -1 if not found
+     */
+    private int findLeadingPattern(String in, String pattern) {
+        digest.reset();
+        int num = 0;
+        for(;;) {
+            var value = md5( in + num);
+            if (value.startsWith(pattern)) {
+                return num;
+            }
+
+            if(num >= Integer.MAX_VALUE) {
+                return -1;
+            }
+
+            num++;
         }
     }
 
     @Override
     public void assertTests() {
-        var keyAnswer = (HashMap<String, String>) Map.of(
+        var keyAnswer = Map.of(
                 "abcdef", "609043",
                 "pqrstuv", "1048970"
         );
 
         keyAnswer.forEach((key, value) -> {
-            findLeadingPattern(key, value);
+            int r = findLeadingPattern(key, "00000");
+            if(r == -1) throw new AssertionError("Key " + key + " not found");
+            if(r != Integer.parseInt(value)) throw new AssertionError("Key " + key + " not equal to value " + value);
         });
-
-
     }
 
 }
