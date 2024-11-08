5a471da6d5b5cbecd03fec6a5b31d63da4d1948b
Kyle Harrison <redactedprofile@gmail.com>
2024-11-08 09:23:57 -0800

    2015:Day9 - wrapping up Day9 to find the shortest path, got all the routes distances calculated, now im just trying to expose the actual route and not just the number in a somewhat efficient way

--- src/main/java/com/redactedprofile/Y2015/days/Day9.java
+++ src/main/java/com/redactedprofile/Y2015/days/Day9.java
@@ -4,9 +4,7 @@ import com.redactedprofile.AOCDay;
 import com.redactedprofile.Shared.Algorithms.Factorial;
 import com.redactedprofile.Shared.Algorithms.Heaps;
 
-import javax.swing.*;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,6 +14,8 @@ public class Day9 extends AOCDay {
     private static final int NAV_FROM = 0;
     private static final int NAV_TO = 1;
     private static final int NAV_DISTANCE = 2;
+
+    private record RouteDistance(String from, String to, int distance) {}
 
 
     @Override
@@ -53,6 +53,14 @@ public class Day9 extends AOCDay {
         action.accept(tokenize(line));
     }
 
+    private List<RouteDistance> generateRouteDistanceMap(List<List<String>> navs) {
+        List<RouteDistance> routeDistances = new ArrayList<>(navs.size());
+        for(List<String> nav : navs) {
+            routeDistances.add(new RouteDistance(nav.get(NAV_FROM), nav.get(NAV_TO), Integer.parseInt(nav.get(NAV_DISTANCE))));
+        }
+        return routeDistances;
+    }
+
     /**
      * Generate all possible permutations of the endpoints
      * @param endpoints
@@ -72,15 +80,51 @@ public class Day9 extends AOCDay {
      * @param navs
      * @return
      */
-    Set<String> collectEndpoints(List<List<String>> navs) {
+    Set<String> collectEndpoints(List<RouteDistance> navs) {
         Set<String> endpoints = new HashSet<>();
 
-        for (List<String> nav : navs) {
-            endpoints.addAll(List.of(nav.get(NAV_FROM), nav.get(NAV_TO)));
+        for (RouteDistance nav : navs) {
+            endpoints.addAll(List.of(nav.from, nav.to));
         }
 
         return endpoints;
     }
+
+    private Integer calculateRouteDistance(List<String> route, List<RouteDistance> routeDistanceMap) {
+        int distance = 0;
+        // Step through each endpoint, getting the distance between the current and the next (if there is one)
+        for(int i = 0; i < route.size() - 1; i++) {
+            String from  = route.get(i),
+                   to = route.get(i+1);
+
+            // look up the distance map
+            for(RouteDistance nav : routeDistanceMap) {
+                // Bi-directional lookup as they'd be the same distance
+                if(nav.from.equals(from) && nav.to.equals(to) || nav.from.equals(to) && nav.to.equals(from)) {
+                    distance += nav.distance;
+                    break;
+                }
+            }
+        }
+
+        return distance;
+    }
+
+    private ArrayList<Integer> calculateRouteDistances(List<List<String>> routes, List<RouteDistance> routeDistanceMap) {
+        ArrayList<Integer> routeDistances = new ArrayList<>(routes.size());
+
+        for(List<String> route : routes) {
+            routeDistances.add(calculateRouteDistance(route, routeDistanceMap));
+        }
+
+        return routeDistances;
+    }
+
+//    private Map<String, Integer> findShortestRoute(List<List<String>> routes, List<Integer> routeDistances) {
+//        // I could easily just quicksort the routeDistances and return the integer, but that doesn't tell me WHICH calculated route is the shorted
+//        // I want to expose, even just for myself, what that route was as well
+//        // So I can either quickly create a hashmap construct that binds these two arrays together,
+//    }
 
 
     @Override
@@ -98,7 +142,9 @@ public class Day9 extends AOCDay {
         if(!tok3.get(NAV_DISTANCE).equals("141")) throw new AssertionError("Assertion 3 failed: tok2[NAV_DIST] != 141: " + tok3.get(NAV_DISTANCE));
 
         List<List<String>> navs = List.of(tok1, tok2, tok3);
-        Set<String> endpoints = collectEndpoints(navs);
+        List<RouteDistance> routeDistanceMap = generateRouteDistanceMap(navs);
+
+        Set<String> endpoints = collectEndpoints(routeDistanceMap);
         if(endpoints.size() != 3) throw new AssertionError("Assertion 4 failed: endpoints.size() != 3: " + endpoints.size());
 
         var routes = collectRoutes(endpoints);
@@ -106,6 +152,11 @@ public class Day9 extends AOCDay {
         int factorial = Factorial.fromInt(endpoints.size());
         if(routes.size() != factorial) throw new AssertionError("Assertion 5 failed: routes.size() != "+factorial+": " + routes.size());
 
+        List<Integer> routeDistances = calculateRouteDistances(routes, routeDistanceMap);
+        if(routeDistances.size() != 6) throw new AssertionError("Assertion 6 failed: routeDistances.size() != 6: " + routeDistances.size());
+
 
     }
+
+
 }
