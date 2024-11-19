0ed2d231974e4b467bf990729a6695f1e7c2e30b
Kyle Harrison <redactedprofile@gmail.com>
2024-11-19 14:52:45 -0800

    2023:Day7 starting

--- src/main/java/com/redactedprofile/Y2023/days/Day7.java
+++ src/main/java/com/redactedprofile/Y2023/days/Day7.java
@@ -3,4 +3,11 @@ package com.redactedprofile.Y2023.days;
 import com.redactedprofile.AOCDay;
 
 public class Day7 extends AOCDay {
+
+
+
+    @Override
+    public void assertTests() {
+        String str1 = "";
+    }
 }
